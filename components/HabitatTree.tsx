0e21737777b8f47517708feedf2a1f246237864d
yannovaes <yanbatata68@gmail.com>
2026-07-06 09:00:55 -0300

    bugfix: consertar renderização da arvore no android

--- components/HabitatTree.tsx
+++ components/HabitatTree.tsx
@@ -4,7 +4,7 @@ import { getLevelFromXp } from "@/lib/domain/xp";
 import { almondLeafPath, organicBlobPath, seededUnit } from "@/lib/organic-paths";
 import { useEcoStore } from "@/store/useEcoStore";
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet } from "react-native";
 import Svg, {
   Defs,
   G,
@@ -218,7 +218,6 @@ export default function HabitatTree({
   const treeTransform = `translate(${210 * (1 - matureScale)} ${742 * (1 - matureScale)}) scale(${matureScale})`;
 
   return (
-    <View style={styles.container}>
       <Svg
         viewBox="0 0 420 760"
         width="100%"
@@ -267,7 +266,6 @@ export default function HabitatTree({
           </G>
         ) : null}
       </Svg>
-    </View>
   );
 }
 
